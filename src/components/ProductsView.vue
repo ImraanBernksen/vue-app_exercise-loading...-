97dec62fe5213c754dd49f4c2a56b81a6363593d
ImraanBernksen <imraanbernksen@gmail.com>
2022-12-19 14:03:54 +0200

    yes

--- src/components/ProductsView.vue
+++ src/components/ProductsView.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>This is an product page</h1>
-    <div>
+    <div v-if="arr.length">
   <div style="display: grid; grid-template-columns: repeat(4, 1fr); margin: auto;">
     <div class="card" v-for="item in arr" :key="item" style="width: 18rem; grid-column: span 1; margin: auto;">
       <img :src="item.imgSrc" class="card-img-top">
@@ -16,10 +16,17 @@
     </div>
   </div>
 </div>
+<div v-else>
+ <Spinner/>
+</div>
 </template>
 <script>
+import Spinner from '../components/SpinnerView.vue';
 export default {
   name: 'Products',
+  components: {
+    Spinner
+  },
   data (){
     return {
   arr : [
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,19 +1,11 @@
 <template>
-   <div v-if="spinner"><Spinner/></div>
-    <div v-else><Products/></div>
+    <div><Products/></div>
 </template>
 <script>
 import Products from "../components/ProductsView.vue";
-import Spinner from "../components/SpinnerView.vue";
 export default {
     components: {
-        Products,
-        Spinner
-    },
-    data (){
-        return {
-            spinner:false
-        }
+        Products
     }
 }
 </script>
